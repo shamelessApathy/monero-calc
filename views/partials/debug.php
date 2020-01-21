d4538ae40e08d0b33ccae83757f699cbcf4dc0a3
shamelessApathy <bcm811@gmail.com>
2020-01-21 08:47:09 -0700

    buttoned up a button

--- views/partials/debug.php
+++ views/partials/debug.php
@@ -1,5 +1,4 @@
 <div id="debug-container" draggable="true">
-    <button id='resize-button'>Resize</button>
     <h3 class="debug-h3"> DEBUG SECTION </h3>
 <?php
 echo "<pre style='color:white;padding:5px;'>";
